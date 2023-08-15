4ad7e30aa4937cc52a003eb465944e19fa27973c
Sajal Chauhan <sajalchauhan2004@gmail.com>
2023-08-15 20:08:46 +0530

    Updated

--- app.py
+++ app.py
@@ -68,17 +68,17 @@ class Interpreter:
             return token
 
         if current_char == '+':
-            token = Token(PLUS, str(current_char))
+            token = Token(PLUS, current_char)
             self.advance()
             return token
         
         if current_char == '-':
-            token = Token(MINUS, str(current_char))
+            token = Token(MINUS, current_char)
             self.advance()
             return token
         
         if current_char == ' ':
-            token = Token(WHITESPACE, str(current_char))
+            token = Token(WHITESPACE, current_char)
             self.advance()
             return token
 
@@ -91,63 +91,49 @@ class Interpreter:
             
         else:
             self.error()
-
-    def arithmetic_operation(self):
-        # initially current token is none, so get hold of first char and token
-        self.current_token = self.get_next_token()
+    
+    def operation(self):
+        str_buffer, result, is_add_operator = '', 0, True  # Initialize is_add_operator to True
         
-        # numbers = []
-        # plus_operator = True
-        # number_string = ''
-        
-        # # input -> '33 + 45'
-        # for char in self.text:
-        #     self.current_token = self.get_next_token()
+        while self.position < len(self.text):
+            char = self.text[self.position]
+            self.position += 1
             
-        #     if self.current_token.type == 'WHITESPACE':
-        #         continue
+            if char == ' ':
+                continue
             
-        #     if self.current_token.type == 'INTEGER':
-        #         number_string += str(self.current_token.value)
-            
-        #     if(self.current_token.type != 'INTEGER'):
-        #         numbers.append(int(number_string))
-        #         number_string = ''
-        #         if(self.current_token.type == 'MINUS'):
-        #             plus_operator = False
+            if char.isdigit():
+                str_buffer += char
                 
-        #     self.eat(self.current_token.type)
-        
-        # # Append the last number after the loop
-        # numbers.append(int(number_string))
+                # Handle multi-digit numbers
+                while self.position < len(self.text) and self.text[self.position].isdigit():
+                    str_buffer += self.text[self.position]
+                    self.position += 1
             
-        # if plus_operator == False:
-        #     return numbers[0] - numbers[1]
-        # else:
-        #     return numbers[0] + numbers[1]
-
-
-        # Expecting the first(leftmost) char to be an integer
-        left = self.current_token
-        self.eat(left.type)
-        
-        # Middle char is expected to be an operator
-        op = self.current_token
-        self.eat(op.type)
+            elif char == '+':
+                if not is_add_operator:
+                    result -= int(str_buffer)
+                else:
+                    result += int(str_buffer)
+                str_buffer = ''
+                is_add_operator = True
+                
+            elif char == '-':
+                if not is_add_operator:
+                    result -= int(str_buffer)
+                else:
+                    result += int(str_buffer)
+                str_buffer = ''
+                is_add_operator = False
+                
+        # Handle the last number in the sequence
+        if str_buffer:
+            if is_add_operator:
+                result += int(str_buffer)
+            else:
+                result -= int(str_buffer)
             
-        # Right char is again expected to be integer
-        right = self.current_token
-        self.eat(right.type)
-        
-        if op.type == 'PLUS':
-            result = left.value + right.value
-            return result
-            
-        elif op.type == 'MINUS':
-            result = left.value - right.value
-            return result
-        
-        self.error()
+        return result
             
     
 def main():
@@ -161,9 +147,10 @@ def main():
         if not text:
             continue
         interpreter = Interpreter(text)
-        result = interpreter.arithmetic_operation()
+        result = interpreter.operation()
         print(result)
 
 
 if __name__ == '__main__':
-    main()
+    main() 
+    
